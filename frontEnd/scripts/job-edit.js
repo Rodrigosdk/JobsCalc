5411ded8cdafa7e821a369009714632848ca75c6
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-04-14 15:53:35 -0300

    Deleted folder frontEnd

--- frontEnd/scripts/job-edit.js
+++
@@ -1,7 +0,0 @@
-import Modal from  'frontEnd/scripts/modal.js';
-
-const modal = Modal({ animateClasses: ['animate-pop', 'back'] })
-
-document
-  .querySelector('.open-modal')
-  .addEventListener('click', modal.open)
