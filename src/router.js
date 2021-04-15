8aa180a4afd1bd1a8cb14a456973edfab84b8c3d
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-04-14 22:44:27 -0300

    New routers

--- src/router.js
+++ src/router.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (requeste, response) => {
-    return response.sendFile(__dirname + '/views/index.html')
-});
+const basePath = __dirname+'/views'
+
+router.get('/', (requeste, response) => response.sendFile(`${basePath}/index.html`));
+router.get('/job', (requeste, response) => response.sendFile(`${basePath}/job.html`));
+router.get('/job/edit', (requeste, response) => response.sendFile(`${basePath}/job-edit.html`));
+router.get('/profile', (requeste, response) => response.sendFile(`${basePath}/profile.html`));
 
 module.exports = router;
