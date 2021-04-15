a0f646d23dd322b45994f4ab4a3f104909cd858e
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-04-14 21:50:20 -0300

    Update

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
-const router =  require('./router/home');
+const router = require('./router');
 
 const app = express();
 
-app.use('/', router);
+app.use(express.static('public'));
+app.use(router)
 
-app.listen(3333,()=>console.log("sever on http://localhost:3333"));
+app.listen(3333, () => console.log("sever on http://localhost:3333"));
