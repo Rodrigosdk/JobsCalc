3e3129314edef79bbcf389ccc098bb94a47d5044
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-11 23:04:19 -0300

    criado rota post de jobs

--- src/router.js
+++ src/router.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 
-const pagesController = require('./controller/pages')
+const pagesController = require("./controller/pages");
 
 const router = express.Router();
 
-router.get('/', pagesController.home);
-router.get('/job', pagesController.jobs);
-router.get('/job/edit', pagesController.jobsEdit);
-router.get('/profile', pagesController.profile);
+router.get("/", pagesController.home);
+router.get("/job", pagesController.jobs);
+router.post("/job", (req,res) => console.log(req));
+router.get("/job/edit", pagesController.jobsEdit);
+router.get("/profile", pagesController.profile);
 
 module.exports = router;
