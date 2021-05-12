7355e720da2d3afd9cfa8f2ee29791055f47eeed
Rodrigo Mamede <rodrigomamede53@gmail.com>
2021-05-12 10:54:04 -0300

    Criado o array de jobs

--- src/router.js
+++ src/router.js
@@ -4,9 +4,14 @@ const pagesController = require("./controller/pages");
 
 const router = express.Router();
 
+const jobs = [];
+
 router.get("/", pagesController.home);
 router.get("/job", pagesController.jobs);
-router.post("/job", (req,res) => console.log(req));
+router.post("/job", (req, res) => {
+  jobs.push(req.body);
+  return res.redirect('/')
+});
 router.get("/job/edit", pagesController.jobsEdit);
 router.get("/profile", pagesController.profile);
 
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { use } = require('./router');
 const router = require('./router');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.urlencoded({extended:true}))
 
 app.use(express.static('public'));
 app.use(router);
